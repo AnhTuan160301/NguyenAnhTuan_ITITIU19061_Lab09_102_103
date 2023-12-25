e5bbd0011969240a3b9ea4862814f35b662b5b79
AnhTuan160301 <74240275+AnhTuan160301@users.noreply.github.com>
2023-12-25 18:58:04 +0700

    Commit for the Lab09. Task 102, 103

---
+++ java/shrine/app/src/main/java/com/google/codelabs/mdc/java/shrine/ProductGridFragment.java
@@ -0,0 +1,69 @@
+package com.google.codelabs.mdc.java.shrine;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.codelabs.mdc.java.shrine.network.ProductEntry;
+
+public class ProductGridFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment with the ProductGrid theme
+        View view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false);
+
+        // Set up the tool bar
+        setUpToolbar(view);
+
+        // Set up the RecyclerView
+        view.recycler_view.setHasFixedSize(true)
+        val gridLayoutManager = GridLayoutManager(context, 2, RecyclerView.HORIZONTAL, false)
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (position % 3 == 2) 2 else 1
+            }
+        }
+        view.recycler_view.layoutManager = gridLayoutManager
+        val adapter = StaggeredProductCardRecyclerViewAdapter(
+                ProductEntry.initProductEntryList(resources))
+        view.recycler_view.adapter = adapter
+        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_large)
+        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_staggered_product_grid_spacing_small)
+        view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
+
+        return view;
+    }
+
+    private void setUpToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.shr_toolbar_menu, menu);
+        super.onCreateOptionsMenu(menu, menuInflater);
+    }
+
+}
